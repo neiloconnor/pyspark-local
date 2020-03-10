efd2dad1f1c5a7fdf913eaeb0a6eaffb4021736a
Neil O'Connor <neil.oconnor@gmail.com>
2020-03-10 10:40:44 +0000

    Add parallelize() to SparkContext

--- example.py
+++ example.py
@@ -1,2 +1,14 @@
+# Based on examples in PySpark cheatsheet
+# https://s3.amazonaws.com/assets.datacamp.com/blog_assets/PySpark_Cheat_Sheet_Python.pdf
+
+# Initalizing Spark
+# SparkContext
 from pyspark import SparkContext
 sc = SparkContext()
+
+# Loading data
+# Parallelized Collections
+rdd = sc.parallelize([('a',7),('a',2),('b',2)])
+rdd2 = sc.parallelize([('a',2),('d',1),('b',1)])
+rdd3 = sc.parallelize(range(100))
+rdd4 = sc.parallelize([("a",["x","y","z"]),  ("b",["p", "r"])])
--- pyspark.py
+++ pyspark.py
@@ -2,3 +2,15 @@
 class SparkContext(object):
     def __init__(self):
         pass
+
+    def parallelize(self, dataset):
+        return RDD(dataset)
+
+
+class RDD(object):
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def collect(self):
+        return self.dataset
+
